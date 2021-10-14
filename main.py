3ab7fa56fbfbffc43f6ec917c0054106a4111ba0
Leglait <evagar.96@gmail.com>
2021-10-14 20:31:56 +0200

    Nuevo dataset generado (14/10/21 20:28)

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import extraccion_datos_ign as edi
 # Función principal
 def main():
-    respuesta = edi.descargaPaginaWeb('https://www.ign.es/web/ign/portal/ultimos-terremotos/-/ultimos-terremotos/get30dias')
+    respuesta = edi.descargaPaginaWeb('https://www.ign.es/web/ign/portal/ultimos-terremotos/-/ultimos-terremotos/getAnio')
     lista = edi.extraerDatos(respuesta)
-    edi.crearArchivoCSVDesdeLista(lista, 'dataset2.csv')
+    edi.crearArchivoCSVDesdeLista(lista, 'dataset_anio.csv')
 
 if __name__ == '__main__':
 	main()
