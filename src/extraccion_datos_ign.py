a5a92af1dc0140df031bc74268a7d17fb15e410e
Leglait <evagar.96@gmail.com>
2021-10-16 13:37:11 +0200

    Cosas puesta en directorios

--- src/extraccion_datos_ign.py
+++ src/extraccion_datos_ign.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 def descargaPaginaWeb(url):
     respuesta = requests.get(
         url,
-        #headers = {
-        #    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
-        #}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
+        }
     )
     return respuesta
     
