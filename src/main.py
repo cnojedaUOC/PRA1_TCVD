e73635a4dc49f011c49b4799a58f333365063e83
Leglait <evagar.96@gmail.com>
2021-10-17 12:58:33 +0200

    nombre cambiado

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import extraccion_datos_ign as edi
 def main():
     respuesta = edi.descargaPaginaWeb('https://www.ign.es/web/ign/portal/ultimos-terremotos/-/ultimos-terremotos/getAnio')
     lista = edi.extraerDatos(respuesta)
-    edi.crearArchivoCSVDesdeLista(lista, '../csv/dataset_anio.csv')
+    edi.crearArchivoCSVDesdeLista(lista, '../csv/seismos_2021.csv')
 
 if __name__ == '__main__':
 	main()
